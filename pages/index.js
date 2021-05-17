b1a433dd374172258f6edc77e9f624c7dbcbd6dc
SherryH <enjoy.wind7rain@gmail.com>
2021-05-17 09:22:42 +0200

    Updated footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { ControlContainer } from '../components/ControlContainer.tsx';
 import { SpeechRecognitionArea } from '../components/SpeechRecognitionArea';
@@ -47,15 +46,8 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+        <a href="https://github.com/SherryH/GermanPreachingApp" target="_blank">
+          Made with 🎤 by Sherry Hsu
         </a>
       </footer>
     </div>
