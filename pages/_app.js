c7d8c70a70ce7d0b7ff779525302755c889cc75d
sherryh <enjoy.wind7rain@gmail.com>
2022-04-24 12:38:39 +0200

    installed styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
 import { Layout } from '../components/Layout';
+import { GlobalStyles } from '../styles/GlobalStyles';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <GlobalStyles />
       <Component {...pageProps} />
     </Layout>
   );
