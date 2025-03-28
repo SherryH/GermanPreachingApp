3a9a0a759d82b1ece8241487943624aa03f45637
SherryH <enjoy.wind7rain@gmail.com>
2025-03-28 21:33:18 +0800

    Make the hideElement work again

--- styles/cssUtils.js
+++ styles/cssUtils.js
@@ -3,5 +3,5 @@ import { css } from 'styled-components';
 // hides element but content still visible to screenreaders
 export const hideElement = css`
   clip: rect(0, 0, 0, 0);
-  position: sticky;
+  position: fixed;
 `;
