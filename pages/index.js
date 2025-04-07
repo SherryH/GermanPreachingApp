15c4388fa300a299a5865e11e495d337056d08f6
SherryH <enjoy.wind7rain@gmail.com>
2025-04-07 18:34:12 +0800

    Fixed Home page main area alignment

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,28 @@ import Head from 'next/head';
 import { ControlContainer } from '../components/ControlContainer';
 import { SpeechRecognitionArea } from '../components/SpeechRecognitionArea';
 
+// I want the Home component to automatically links to Lesson 10 with url ending "/10"
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>Lektion 4 Preaching Exercise</h1>
+    <>
+      <h1>German Preaching Exercise</h1>
 
-        <p>0. Please use Chrome browser for the best experience</p>
-        <p>
-          1. Click "Trigger" to get a random combination of preaching vocabs
-        </p>
-        <p>
-          2.Click on the 🎤 to say out loud the preaching sentences using
-          triggers
-        </p>
-        <p>3. Observe the transcribed sentences</p>
-        <p>4. Click on "Answers" to see answers and compare</p>
-        <p>
-          5. Click on the provided answers and listen to the correct
-          pronounciation
-        </p>
+      <p>0. Please use Chrome browser for the best experience</p>
+      <p>1. Click "Trigger" to get a random combination of preaching vocabs</p>
+      <p>
+        2.Click on the 🎤 to say out loud the preaching sentences using triggers
+      </p>
+      <p>3. Observe the transcribed sentences</p>
+      <p>4. Click on "Answers" to see answers and compare</p>
+      <p>
+        5. Click on the provided answers and listen to the correct
+        pronounciation
+      </p>
+      <p>See Below as an example</p>
 
-        <div className="controlWrapper">
-          <ControlContainer />
-        </div>
-      </main>
-    </div>
+      <div className="controlWrapper">
+        <ControlContainer />
+      </div>
+    </>
   );
 }
